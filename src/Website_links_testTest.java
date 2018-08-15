f0986b388725dd74add21911c63073b06a212746
Andy C <ajcuc010@gmail.com>
2018-08-15 16:50:59 -0700

    Edited code to remove repetition of code

--- src/Website_links_testTest.java
+++ src/Website_links_testTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,33 +15,46 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class Website_links_testTest {
 
+    WebDriver browser;
+    static final String driverKey = "webdriver.chrome.driver";
+    static final String driverValue = "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe";
+    static final String homePage = "https://www.gamblitgaming.com";
 
-    @Test
-    void logo_link_works() {
-        WebDriver browser;
+    @BeforeAll
+    static void setup() {
+        System.setProperty(driverKey, driverValue);
+    }
 
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
+    @BeforeEach
+    void setup2() {
         browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com");
-        WebElement gamblit_logo_image_link = browser.findElement(By.className("logo"));
-        gamblit_logo_image_link.click();
-        browser.quit();
     }
 
     @Test
+    void logo_link_works() {
+        browser.manage().window().maximize();
+        browser.get(homePage);
+        WebElement gamblit_logo_image_link = browser.findElement(By.className("logo"));
+        gamblit_logo_image_link.click();
+        Assertions.assertEquals("https://gamblitgaming.com/", browser.getCurrentUrl(), "URLs for homepage do not match!");
+    }
+
+    // GLI page opens in a new tab, affecting the results... already an unusual test...
+    @Test
     void GLI_link_works() {
         // this works when using a workaround of using Thread.sleep(), but doesn't work when going without it
-        WebDriver browser;
 
         try {
-            System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-            browser = new ChromeDriver();
-            browser.get("https://www.gamblitgaming.com");
+            //
+            browser.manage().window().maximize();
+            browser.get(homePage);
             WebElement GLI_image_link = browser.findElement(By.xpath("//img[@ src='/assets/images/glc-badge.jpg']"));
             GLI_image_link.click();
             Thread.sleep(5000);
-            //browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            browser.quit();
+            browser.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "\t");
+            //Assertions.assertEquals("Gamblit Gaming, LLC", browser.getTitle(), "URLs for GLI page do not match!");
+            browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            Assertions.assertEquals("https://www.gaminglabs.com/extranetGLI/cmsAdmin/getsplash.asp?i=161", browser.getCurrentUrl(), "URLs for GLI page do not match!");
         } catch (Exception e) {
 
         }
@@ -49,156 +63,127 @@ class Website_links_testTest {
 
     @Test
     void about_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement about_link = browser.findElement(By.linkText("ABOUT"));
         about_link.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/about/", browser.getCurrentUrl(), "URLs for About page do not match!");
     }
 
     @Test
     void developers_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement developers_link = browser.findElement(By.linkText("DEVELOPERS"));
         developers_link.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/developers/", browser.getCurrentUrl(), "URLs for Developers page do not match!");
     }
 
     @Test
     void operators_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+//        browser = new ChromeDriver();
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement operators_link = browser.findElement(By.linkText("OPERATORS"));
         operators_link.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/operators/", browser.getCurrentUrl(), "URLs for Operators page do not match!");
     }
 
     @Test
     void games_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement games_link = browser.findElement(By.linkText("GAMES"));
         games_link.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/games/", browser.getCurrentUrl(), "URLs for Game page do not match!");
     }
 
     @Test
     void news_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement news_link = browser.findElement(By.linkText("NEWS"));
         news_link.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/blog/", browser.getCurrentUrl(), "URLs for Blog/News page do not match!");
     }
 
     @Test
     void contact_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
         browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement contact_link = browser.findElement(By.linkText("CONTACT"));
         contact_link.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/contact/", browser.getCurrentUrl(), "URLs for Contact page do not match!");
     }
 
     @Test
     void responsible_gaming_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement responsible_gaming_link = browser.findElement(By.partialLinkText("Responsible"));
         responsible_gaming_link.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/responsible/", browser.getCurrentUrl(), "URLs for Responsible Gaming page do not match!");
     }
 
     @Test
     void careers_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement careers_link = browser.findElement(By.partialLinkText("Careers"));
         careers_link.click();
 
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        Assertions.assertEquals("https://gamblitgaming.com/about/careers/", browser.getCurrentUrl(), "URLs for Career page");
     }
 
-    @Test
+    // Facebook opens in a new tab, affecting the results...
+    @Disabled
     void facebook_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement facebook_image_link = browser.findElement(By.partialLinkText("Facebook"));
         facebook_image_link.click();
-        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        //browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //browser.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "\t");
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //Assertions.assertEquals("facebook.com/gamblitgaming", browser.getCurrentUrl(), "URLs for Facebook do not match!");
     }
 
-    @Test
+    // Twitter opens in a new tab, affecting the results...
+    @Disabled
     void twitter_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com");
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement twitter_image_link = browser.findElement(By.partialLinkText("Twitter"));
         twitter_image_link.click();
-        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        browser.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "\t");
+        Assertions.assertEquals("twitter.com/gamblitgaming", browser.getCurrentUrl(), "URLs for Twitter do not match!");
     }
 
     @Disabled
     void another_link_works() {
         // don't know why I have this test
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com");
+//        browser = new ChromeDriver();
+        browser.manage().window().maximize();
+        browser.get(homePage);
         WebElement facebook = browser.findElement(By.partialLinkText("another"));
         facebook.click();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.quit();
     }
 
     @Test
     void about_team_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
 
         Actions actions = new Actions(browser);
         WebElement about_link = browser.findElement(By.linkText("ABOUT"));
@@ -208,17 +193,14 @@ class Website_links_testTest {
         WebElement team_link = browser.findElement(By.partialLinkText("TEAM"));
         //WebElement team_link = browser.findElement(By.xpath("//html/body/header/div[2]/div/nav/ul/li[1]/a")); // xPath method
         actions.moveToElement(team_link).click().build().perform();
-        browser.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        browser.quit();
+        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        Assertions.assertEquals("https://gamblitgaming.com/about/team/", browser.getCurrentUrl(), "URLs for About -> Team page do not match!");
     }
 
     @Test
     void about_careers_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
 
         Actions actions = new Actions(browser);
         WebElement about_link = browser.findElement(By.linkText("ABOUT"));
@@ -227,17 +209,15 @@ class Website_links_testTest {
 
         WebElement career_link = browser.findElement(By.partialLinkText("CAREERS"));
         actions.moveToElement(career_link).click().build().perform();
-        browser.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        browser.quit();
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Assertions.assertEquals("https://gamblitgaming.com/about/careers/", browser.getCurrentUrl(), "URLs for About -> Careers page do not match!");
+
     }
 
     @Test
     void about_where_to_play_link_works() {
-        WebDriver browser;
-
-        System.setProperty("webdriver.chrome.driver", "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe");
-        browser = new ChromeDriver();
-        browser.get("https://www.gamblitgaming.com/");
+        browser.manage().window().maximize();
+        browser.get(homePage);
 
         Actions actions = new Actions(browser);
         WebElement about_link = browser.findElement(By.linkText("ABOUT"));
@@ -246,7 +226,12 @@ class Website_links_testTest {
 
         WebElement where_to_play_link = browser.findElement(By.partialLinkText("WHERE TO PLAY"));
         actions.moveToElement(where_to_play_link).click().build().perform();
-        browser.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Assertions.assertEquals("https://gamblitgaming.com/about/where-to-play/", browser.getCurrentUrl(), "URLs for About -> Where-to-Play page do not match!");
+    }
+
+    @AfterEach
+    void tearDown() {
         browser.quit();
     }
 }
---
+++ src/news_dropdowns_test.java
@@ -0,0 +1,120 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.text.*;
+
+public class news_dropdowns_test {
+    WebDriver browser;
+    static final String chromeDriverKey = "webdriver.chrome.driver";
+    static final String chromeDriverValue = "E:/Applications/Development/AutomatedTestingSW/chromedriver.exe";
+    static final String homePage = "https://www.gamblitgaming.com/blog/";
+
+    @BeforeAll
+    static void setup() {
+        System.setProperty(chromeDriverKey, chromeDriverValue);
+    }
+
+    @BeforeEach
+    void setup2() {
+        browser = new ChromeDriver();
+    }
+
+    @Test
+    void categories_dropdown_option_announcements_works() {
+        browser.get(homePage);
+        WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
+
+        // click on the Category dropdown button...
+        categoryDropdownlist.click();
+
+        // click on an option...
+        WebElement category_dropdownitem_Announcements = browser.findElement(By.partialLinkText("Announcements"));
+        category_dropdownitem_Announcements.click();
+        // assert statement here...
+    }
+
+    @Test
+    void categories_dropdown_option_events_works() {
+        browser.get(homePage);
+        WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
+
+        // click on the Category dropdown button...
+        categoryDropdownlist.click();
+
+        // click on an option...
+        WebElement category_dropdownitem_Announcements = browser.findElement(By.partialLinkText("Events"));
+        category_dropdownitem_Announcements.click();
+
+        // assert statement here...
+    }
+
+    @Test
+    void categories_dropdown_option_press_works() {
+        browser.get(homePage);
+        WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
+
+        // click on the Category dropdown button...
+        categoryDropdownlist.click();
+
+        // click on an option...
+        WebElement category_dropdownitem_Announcements = browser.findElement(By.partialLinkText("Press"));
+        category_dropdownitem_Announcements.click();
+
+        // assert statement here...
+    }
+
+    @Test
+    void categories_dropdown_option_uncategorized_works() {
+        browser.get(homePage);
+        WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
+
+        // click on the Category dropdown button...
+        categoryDropdownlist.click();
+
+        // click on an option...
+        WebElement category_dropdownitem_Announcements = browser.findElement(By.partialLinkText("Uncategorized"));
+        category_dropdownitem_Announcements.click();
+
+        // assert statement here...
+    }
+
+    @Test
+    void archives_dropdown_works()
+    {
+        browser.get(homePage);
+        WebElement archivesDropdownlist = browser.findElement(By.id("archives-dropdown"));
+        archivesDropdownlist.click();
+
+        // assert statement here...
+    }
+
+    @AfterEach
+    void tearDown() {
+        browser.quit();
+    }
+
+    /*String monthtext = "September";
+    int yeartext = 2015;
+
+    @Test
+    void archives_dropdown_option_works(int year, String month){
+        browser = new ChromeDriver();
+        browser.get(homePage);
+        WebElement archivesDropdownlist = browser.findElement(By.id("archives-dropdown"));
+        archivesDropdownlist.click();
+
+        WebElement archivesOption = browser.findElement(By.partialLinkText(month + " " + Integer.toString(year)));
+        archivesOption.click();
+        browser.quit();
+    }*/
+}
