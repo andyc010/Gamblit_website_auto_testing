e40c8c3573e533dce24a4277243237c35fe24cfc
Andy C <arsdatandy@aim.com>
2018-12-31 13:38:57 -0800

    Added assertions & got the test regarding news for particular date working

--- src/news_dropdowns_test.java
+++ src/news_dropdowns_test.java
@@ -32,6 +32,7 @@ public class news_dropdowns_test {
 
     @Test
     void categories_dropdown_option_announcements_works() {
+        browser.manage().window().maximize();
         browser.get(homePage);
         WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
 
@@ -42,10 +43,12 @@ public class news_dropdowns_test {
         WebElement category_dropdownitem_Announcements = browser.findElement(By.partialLinkText("Announcements"));
         category_dropdownitem_Announcements.click();
         // assert statement here...
+        Assertions.assertEquals("https://gamblitgaming.com/blog/categories/announcements", browser.getCurrentUrl(), "URLs for News: Announcements page do not match!");
     }
 
     @Test
     void categories_dropdown_option_events_works() {
+        browser.manage().window().maximize();
         browser.get(homePage);
         WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
 
@@ -57,10 +60,12 @@ public class news_dropdowns_test {
         category_dropdownitem_Announcements.click();
 
         // assert statement here...
+        Assertions.assertEquals("https://gamblitgaming.com/blog/categories/events", browser.getCurrentUrl(), "URLs for News: Events page do not match!");
     }
 
     @Test
     void categories_dropdown_option_press_works() {
+        browser.manage().window().maximize();
         browser.get(homePage);
         WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
 
@@ -72,10 +77,12 @@ public class news_dropdowns_test {
         category_dropdownitem_Announcements.click();
 
         // assert statement here...
+        Assertions.assertEquals("https://gamblitgaming.com/blog/categories/press", browser.getCurrentUrl(), "URLs for News: Press page do not match!");
     }
 
     @Test
     void categories_dropdown_option_uncategorized_works() {
+        browser.manage().window().maximize();
         browser.get(homePage);
         WebElement categoryDropdownlist = browser.findElement(By.id("cat-dropdown"));
 
@@ -87,35 +94,39 @@ public class news_dropdowns_test {
         category_dropdownitem_Announcements.click();
 
         // assert statement here...
+        Assertions.assertEquals("https://gamblitgaming.com/blog/categories/uncategorized", browser.getCurrentUrl(), "URLs for News: Uncategorized page do not match!");
     }
 
     @Test
     void archives_dropdown_works()
     {
+        browser.manage().window().maximize();
         browser.get(homePage);
         WebElement archivesDropdownlist = browser.findElement(By.id("archives-dropdown"));
         archivesDropdownlist.click();
 
         // assert statement here...
+        Assertions.assertEquals("https://gamblitgaming.com/blog/", browser.getCurrentUrl(), "URLs for News w/Archives page do not match!");
+    }
+    
+    // TODO: need to edit this test to perhaps test more than one date/page
+    @Test
+    void archives_dropdown_option_works(){
+        String monthtext = "April";
+        int yeartext = 2016;
+        browser.manage().window().maximize();
+        browser.get(homePage);
+        WebElement archivesDropdownlist = browser.findElement(By.id("archives-dropdown"));
+        archivesDropdownlist.click();
+
+        WebElement archivesOption = browser.findElement(By.partialLinkText(monthtext + " " + Integer.toString(yeartext)));
+        archivesOption.click();
+
+        Assertions.assertEquals("https://gamblitgaming.com/blog/2016/04", browser.getCurrentUrl(), "URLs for News from 11/2013 page do not match!");
     }
 
     @AfterEach
     void tearDown() {
         browser.quit();
     }
-
-    /*String monthtext = "September";
-    int yeartext = 2015;
-
-    @Test
-    void archives_dropdown_option_works(int year, String month){
-        browser = new ChromeDriver();
-        browser.get(homePage);
-        WebElement archivesDropdownlist = browser.findElement(By.id("archives-dropdown"));
-        archivesDropdownlist.click();
-
-        WebElement archivesOption = browser.findElement(By.partialLinkText(month + " " + Integer.toString(year)));
-        archivesOption.click();
-        browser.quit();
-    }*/
 }
